205f0025ebde14fe1ffa6881ecf450fc24dcb5dc
enghonorios <154471089+enghonorios@users.noreply.github.com>
2023-12-22 12:19:40 -0300

    Add files via upload

---
+++ Desafio 4 e 5.py
@@ -0,0 +1,16 @@
+# Desafio 1 e 2 fazendo conta com dados
+
+n1= int(input('Digite valor: '))
+n2= int(input('Outro valor: '))
+soma= n1+n2
+print('A soma de {}+{} é igual, {}'.format(n1,n2,soma))
+
+# Segundo desafio utilizar cola da aula ex005
+
+a= input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('È uma letra maíuscula?' , a.isupper())
+print('È uma letra minuscula?' , a.islower())
+print('É alfa númerico?' , a.isalnum())
+print('Tem somente letras?' , a.isalpha())
+print('Foi digitado somente numeral?' , a.isnumeric())
